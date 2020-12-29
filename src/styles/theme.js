664fdb9b12a5a9bc795ae5bc59addfc6db9373a8
Pedro Mihael <pedromvpaes@gmail.com>
2020-12-29 15:17:51 -0400

    chore: update global theme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,11 +1,13 @@
 const theme = {
-  colors: {
-    darkGray: "#212121",
-    lightGray: "#e5e5e5",
-    mediumLightGray: "#848484",
-    mediumDarkGray: "#333333",
-    iceWhite: "#f1f1f1",
-    saphire: "#3e6680"
+  light: {
+    colors: {
+      darkGray: "#212121",
+      lightGray: "#e5e5e5",
+      mediumLightGray: "#848484",
+      mediumDarkGray: "#333333",
+      iceWhite: "#f1f1f1",
+      white: "#FFFFFF"
+    }
   }
 };
 
