db7724d85e21b270f8af5489bb05bb8de5fa2c0b
Pedro Mihael <pedromvpaes@gmail.com>
2020-12-28 21:35:14 -0400

    update avatar image display method

--- src/components/Chip/styles.js
+++ src/components/Chip/styles.js
@@ -20,16 +20,11 @@ export const Content = styled.div`
 `;
 
 export const Avatar = styled.div`
-  background: ${(props) => props.theme.colors.saphire};
+  background: url(${(props) => props.avatar});
+  background-size: contain;
   border-radius: 100%;
   height: 25px;
   width: 25px;
-
-  img {
-    border-radius: 100%;
-    height: 100%;
-    width: 100%;
-  }
 `;
 
 export const CloseIcon = styled.button`
