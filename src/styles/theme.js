191b4b3fcfa8a31fbf0a33df6fc872432f4b75cc
Pedro Mihael <pedromvpaes@gmail.com>
2020-12-28 21:21:26 -0400

    chore: add colors to theme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,7 +1,11 @@
 const theme = {
   colors: {
     darkGray: "#212121",
-    iceWhite: "#f1f1f1"
+    lightGray: "#e5e5e5",
+    mediumLightGray: "#848484",
+    mediumDarkGray: "#333333",
+    iceWhite: "#f1f1f1",
+    saphire: "#3e6680"
   }
 };
 
